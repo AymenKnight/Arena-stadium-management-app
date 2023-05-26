b3283ab9d76b7f6c5ed8980508f41e4db28fa5b9
Daouadji Aymen <daouadji506@gmail.com>
2023-05-26 01:11:00 +0100

    ✨ use Clerk auth Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,10 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "@/utils/api";
 import "@/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <ClerkProvider {...pageProps}  ><Component {...pageProps} /></ClerkProvider> ;
 };
 
 export default api.withTRPC(MyApp);
